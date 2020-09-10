b82291b5232f30bbb4d979c84a095f920ad9b026
rvsingh3213 <rvsingh3213@gmail.com>
2020-09-10 15:29:10 +0530

    app added with screenshots

---
+++ InkaRestaurent/app/src/main/java/com/inkarestaurent/MainActivity.java
@@ -0,0 +1,287 @@
+package com.inkarestaurent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private BottomNavigationView bottomNavigationView;
+    private  Button one_add,one_sub;private TextView one_txt;
+    private  Button two_add,two_sub;private TextView two_txt;
+    private  Button three_add,three_sub;private TextView three_txt;
+
+    //add buttons
+    private Button one_add_btn,two_add_btn,three_add_btn;
+    //elegant buttons
+   private CardView one_btn,two_btn,three_btn;
+
+    //Cart Total items
+    TextView cartToalItems;
+
+    //TotalCostCard
+
+    private Button main_one;
+    private int totalItems;
+
+    //count for showMore
+
+    private int countShow=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String TAG=getClass().getName().toString();
+        bottomNavigationView=findViewById(R.id.cartNavigationBar);
+
+        one_add=findViewById(R.id.elegant_btn_add);one_sub=findViewById(R.id.elegant_btn_sub); one_txt=findViewById(R.id.elegant_btn_txt);
+        two_add=findViewById(R.id.elegant2_btn_add);two_sub=findViewById(R.id.elegant2_btn_sub);two_txt=findViewById(R.id.elegant2_btn_txt);
+        three_add=findViewById(R.id.elegant3_btn_add);three_sub=findViewById(R.id.elegant3_btn_sub);three_txt=findViewById(R.id.elegant3_btn_txt);
+
+        //add buttons
+        one_add_btn=findViewById(R.id.one_add_btn);
+        two_add_btn=findViewById(R.id.two_add_btn);
+        three_add_btn=findViewById(R.id.three_add_btn);
+
+        //elegant btns
+       one_btn=findViewById(R.id.one_btn);
+        two_btn=findViewById(R.id.two_btn);
+        three_btn=findViewById(R.id.three_btn);
+
+        //cart Total items
+        cartToalItems=findViewById(R.id.cart_total_items);
+        String line[]=cartToalItems.getText().toString().trim().split("\\s+");
+         totalItems = Integer.parseInt(line[1]);
+
+         //count for show more
+        countShow=0;
+
+
+        // when count is 0 and three buttons ADD
+         one_add_btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 totalItems++;
+                 countShow++;
+                 cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+                 one_add_btn.setVisibility(View.INVISIBLE);
+                 one_txt.setText(String.valueOf(1));
+                 one_btn.setVisibility(View.VISIBLE);
+
+             }
+         });
+
+         two_add_btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 totalItems++;
+                 countShow++;
+                 cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+                 two_add_btn.setVisibility(View.INVISIBLE);
+                 two_txt.setText(String.valueOf(1));
+                 two_btn.setVisibility(View.VISIBLE);
+             }
+         });
+
+         three_add_btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 totalItems++;
+                 countShow++;
+                 cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+                 three_add_btn.setVisibility(View.INVISIBLE);
+                 three_txt.setText(String.valueOf(1));
+                 three_btn.setVisibility(View.VISIBLE);
+             }
+         });
+
+
+        one_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(one_txt.getText().toString().trim());
+                if(count<20) {
+                    count++;
+                    one_txt.setText(String.valueOf(count));
+                    totalItems++;
+                    cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+                }
+            }
+        });
+        one_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(one_txt.getText().toString().trim());
+
+                if(count>0)
+                {
+                    count--;
+                    totalItems--;
+                    one_txt.setText(String.valueOf(count));
+                    cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+
+                    if(count==0)
+                    {   countShow--;
+
+                        one_btn.setVisibility(View.INVISIBLE);
+                        one_add_btn.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        });
+
+        two_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(two_txt.getText().toString().trim());
+                if(count<20) {
+                    count++;
+                    totalItems++;
+                    two_txt.setText(String.valueOf(count));
+                    cartToalItems.setText(" ( " + totalItems + " ITEMS)");
+                }
+            }
+        });
+
+        two_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(two_txt.getText().toString().trim());
+
+                if(count>0)
+                {
+                    count--;totalItems--;
+                    two_txt.setText(String.valueOf(count));
+                    cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+
+                    if(count==0)
+                    {   countShow--;
+
+                        two_btn.setVisibility(View.INVISIBLE);
+                        two_add_btn.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        });
+
+        three_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(three_txt.getText().toString().trim());
+                if(count<20) {
+                    count++;
+                    totalItems++;
+                    three_txt.setText(String.valueOf(count));
+                    cartToalItems.setText(" ( " + totalItems + " ITEMS)");
+                }
+            }
+        });
+
+
+        three_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(three_txt.getText().toString().trim());
+
+                if(count>0)
+                {
+                    count--;totalItems--;
+                    three_txt.setText(String.valueOf(count));
+                    cartToalItems.setText(" ( "+totalItems+" ITEMS)");
+
+                    if(count==0)
+                    {   countShow--;
+                        three_btn.setVisibility(View.INVISIBLE);
+                        three_add_btn.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        bottomNavigationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"RV: navbar clicked");
+                bottomNavigationClickMethod();
+
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private void bottomNavigationClickMethod(){
+        String TAG=getClass().getName().toString();
+        String all_item_val=countItemString();
+        String countItem=countItem();
+
+
+        Intent intent=new Intent(getApplicationContext(),placeOrder.class);
+        intent.putExtra("totalCount",String.valueOf(totalItems));
+        intent.putExtra("countItem",String.valueOf(countItem));
+        intent.putExtra("itemsString",all_item_val);
+        Log.i(TAG,"Sttring:"+all_item_val);
+
+
+        startActivity(intent);
+    }
+
+    private String countItem()
+    {    int count=0;
+
+        int one=Integer.parseInt(one_txt.getText().toString());
+        int two=Integer.parseInt(two_txt.getText().toString());
+        int three=Integer.parseInt(three_txt.getText().toString());
+
+        if(one>0)
+            count++;
+        if(two>0)
+            count++;
+        if(three>0)
+            count++;
+
+        return String.valueOf(count);
+    }
+    private String countItemString(){
+        String s="";
+        String val="";
+        int one=Integer.parseInt(one_txt.getText().toString());
+        int two=Integer.parseInt(two_txt.getText().toString());
+        int three=Integer.parseInt(three_txt.getText().toString());
+
+        if(one>0) {
+            s = "1" +" "+String.valueOf(one)+" ";
+
+        }
+        if(two>0) {
+            s += "2" +" "+String.valueOf(two)+" ";
+
+        }
+        if(three>0) {
+            s += "3" +" "+String.valueOf(three)+" ";
+
+        }
+        return s;
+
+
+
+    }
+}
---
+++ InkaRestaurent/app/src/main/java/com/inkarestaurent/placeOrder.java
@@ -0,0 +1,232 @@
+package com.inkarestaurent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class placeOrder extends AppCompatActivity {
+
+
+    //Intent to get values for last Intent
+    private Intent intent;
+    private TextView totalCost;
+    private int totalItems;
+
+    //TextView all quantity
+    private TextView first_item_count,second_item_count,third_item_count;
+
+    //Buttons for quantity
+    private Button first_item_add,first_item_sub,second_item_add,second_item_sub,third_item_add,third_item_sub;
+
+    //TextView showmore
+    TextView showMore;
+    //Recycler cart Items
+    RecyclerView recyclerView;
+
+    //Relative Layout
+    private RelativeLayout first_item_cart,second_item_cart,third_item_cart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_order);
+
+        showMore=findViewById(R.id.cart_show_more);
+        first_item_cart=findViewById(R.id.first_item_cart);
+        second_item_cart=findViewById(R.id.second_item_cart);
+        third_item_cart=findViewById(R.id.third_item_cart);
+
+        //TextView Quantity
+        first_item_count=findViewById(R.id.first_item_count);
+        second_item_count=findViewById(R.id.second_item_count);
+        third_item_count=findViewById(R.id.third_item_count);
+
+        //count quantity buttons
+
+        first_item_add=findViewById(R.id.first_item_add);
+        first_item_sub=findViewById(R.id.first_item_sub);
+        second_item_add=findViewById(R.id.second_item_add);
+        second_item_sub=findViewById(R.id.second_item_sub);
+        third_item_add=findViewById(R.id.third_item_add);
+        third_item_sub=findViewById(R.id.third_item_sub);
+
+
+
+        showMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                third_item_cart.setVisibility(View.VISIBLE);
+                showMore.setVisibility(View.INVISIBLE);
+            }
+        });
+        first_item_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(first_item_count.getText().toString());
+
+                if(count<20)
+                {
+                    count++;
+                    totalItems++;
+                 
+                    first_item_count.setText(String.valueOf(count));
+                }
+            }
+        });
+        first_item_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(first_item_count.getText().toString());
+                totalItems--;
+
+                if(count==1)
+                {
+                    first_item_cart.setVisibility(View.GONE);
+
+                }
+                count--;
+                first_item_count.setText(String.valueOf(count));
+            }
+        });
+
+        //for second
+        second_item_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(second_item_count.getText().toString());
+
+                if(count<20)
+                {
+                    count++;
+                    totalItems++;
+
+                    second_item_count.setText(String.valueOf(count));
+                }
+            }
+        });
+        second_item_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(second_item_count.getText().toString());
+                totalItems--;
+
+                if(count==1)
+                {
+                    second_item_cart.setVisibility(View.GONE);
+
+                }
+                count--;
+                second_item_count.setText(String.valueOf(count));
+            }
+        });
+        // for third
+
+        third_item_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(third_item_count.getText().toString());
+
+                if(count<20)
+                {
+                    count++;
+                    totalItems++;
+
+                    third_item_count.setText(String.valueOf(count));
+                }
+            }
+        });
+        third_item_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count=Integer.parseInt(third_item_count.getText().toString());
+                totalItems--;
+               // totalCost.setText("€"+String.valueOf(totalItems*7)+".00"); if(count==1)
+                {
+                    third_item_cart.setVisibility(View.GONE);
+
+                }
+                count--;
+                third_item_count.setText(String.valueOf(count));
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String TAG=getClass().getName().toString();
+        //intent
+        intent=getIntent();
+        totalCost=findViewById(R.id.total_cost_txt);
+        int totalCount=Integer.parseInt(intent.getStringExtra("totalCount"));
+        int totalItem=Integer.parseInt(intent.getStringExtra("countItem"));
+        totalItems=totalItem;
+        String itemString=intent.getStringExtra("itemsString");
+
+
+        totalCost.setText("€"+String.valueOf(totalCount*7)+".00");
+
+        if(totalItem>2)
+        {
+            showMore.setVisibility(View.VISIBLE);
+        }
+
+        String str[]=itemString.trim().split("\\s+");
+        Log.i(TAG,"given is::"+itemString);
+        int itemArray[]=new int[str.length];
+
+        for(int i=0;i<totalItem*2;i++) {
+            itemArray[i] = Integer.parseInt(str[i]);
+        }
+        for(int i=0;i<totalItem*2;i+=2)
+        {
+            if(itemArray[i]==1)
+            {
+                first_item_cart.setVisibility(View.VISIBLE);
+                Log.i(TAG,"set::1:"+String.valueOf(itemArray[i+1]));
+                first_item_count.setText(String.valueOf(itemArray[i+1]));
+
+            }
+            if(itemArray[i]==2)
+            {
+                second_item_cart.setVisibility(View.VISIBLE);
+                Log.i(TAG,"set::2:"+String.valueOf(itemArray[i+1]));
+                second_item_count.setText(String.valueOf(itemArray[i+1]));
+            }
+
+            if(itemArray[i]==3)
+            {     Log.i(TAG,"set::3:"+String.valueOf(itemArray[i+1]));
+                third_item_count.setText(String.valueOf(itemArray[i+1]));
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+
+
+    }
+}
